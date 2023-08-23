2dc7ff946fd18ac181cd6ecfff11915529f0f174
zhangyongxiang <zhangyongxiang@baidu.com>
2023-08-23 19:43:03 +0800

    pdate

--- httpwriter/src/main/java/com/alibaba/datax/plugin/writer/httpwriter/HttpWriter.java
+++ httpwriter/src/main/java/com/alibaba/datax/plugin/writer/httpwriter/HttpWriter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.alibaba.datax.common.plugin.RecordReceiver;
 import com.alibaba.datax.common.spi.Writer;
 import com.alibaba.datax.common.util.Configuration;
+import com.google.common.collect.Lists;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,32 +20,33 @@ public class HttpWriter extends Writer {
         
         @Override
         public void preCheck() {
-            super.preCheck();
+            log.info("pre check");
         }
         
         @Override
         public void prepare() {
-            super.prepare();
+            log.info("pre check");
         }
         
         @Override
         public void post() {
-            super.post();
+            log.info("pre check");
         }
         
         @Override
         public void init() {
-            
+            log.info("pre check");
         }
         
         @Override
         public void destroy() {
-            
+            log.info("pre check");
         }
         
         @Override
         public List<Configuration> split(final int mandatoryNumber) {
-            return null;
+            final Configuration configurations = this.getPluginJobConf();
+            return Lists.newArrayList(configurations);
         }
     }
     
@@ -53,17 +55,18 @@ public class HttpWriter extends Writer {
         
         @Override
         public void init() {
-            
+            log.info("task init");
         }
         
         @Override
         public void destroy() {
-            
+            log.info("task destroy");
         }
         
         @Override
         public void startWrite(final RecordReceiver lineReceiver) {
-            
+            log.info(lineReceiver.getFromReader().toString());
+            System.out.println(lineReceiver.getFromReader().toString());
         }
     }
 }
